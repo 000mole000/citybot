257e13362b3bd46b076d91ab49205fa7dfdc46a1
000mole000 <andreymole@mail.ru>
2019-11-16 17:52:41 +0300

    Добавлена команда принять

--- commands.py
+++ commands.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 from config import bot as config_bot
 from db import open_db, update_db
@@ -33,9 +34,9 @@ async def drink_order(ctx, drink):
         if user["coin"] >= drinks[drink]["cost"]:
             order = data_bar["order"]
             if user_name not in data_bar["order"]:
-                order[user_name] = []
-            order[user_name].append(drink)
-            update_db("bar.json", data_bar)
+                order[user_name] = ''
+            order[user_name] = drink
+            update_db("database/bar.json", data_bar)
             await ctx.send(ctx.author.name + ' заказал ' + drink)
             return
     menu = ''
@@ -43,6 +44,34 @@ async def drink_order(ctx, drink):
         drink_menu_str = str(drink_menu)
         menu = menu + drink_menu_str + ': ' + str(drinks[drink_menu_str]["cost"]) + '\n'
     await ctx.send(ctx.author.name + ': \n' + menu)
+
+
+@bot.command(name='принять')
+@commands.has_role("бармен")
+async def drink_accept(ctx, user: discord.Member):
+    data = open_db("database/passports.json")
+    data_bar = open_db("database/bar.json")
+    items = open_db("database/items.json")
+    client = str(user)
+    if client in data_bar["order"]:
+        order = data_bar["order"][client]
+        if data[client]["coin"] >= items["drinks"][str(order)]["cost"]:
+            print(order)
+            data[client]["coin"] -= items["drinks"][str(order)]["cost"]
+            update_db('database/passports.json', data)
+            order2 = data_bar["order"].pop(client)
+            update_db('database/bar.json', data_bar)
+            await ctx.send('Заказ ' + client + 'оформлен(' + str(order2) + ')')
+        else:
+            await ctx.send('У '+client+' не достаточно средств')
+    else:
+        await ctx.send('Пользователь ничего не заказывал (Стёпа - дэбил)')
+
+
+@drink_accept.error
+async def info_error(ctx, error):
+    if isinstance(error, commands.MissingRole):
+        await ctx.send('Вы не бармен')
 
 
 bot.run(config_bot['token'])
