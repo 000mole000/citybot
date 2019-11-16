a23287a385a9b40f841ef3749c81f85017cebfa7
000mole000 <andreymole@mail.ru>
2019-11-16 14:30:38 +0300

    добавлен заказ

--- commands.py
+++ commands.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 from config import bot as config_bot
+from db import open_db, update_db
 import json
 
 bot = commands.Bot(command_prefix=config_bot['prefix'])
@@ -7,23 +8,41 @@ bot = commands.Bot(command_prefix=config_bot['prefix'])
 
 @bot.command(name='паспорт')
 async def passport(ctx):
-    passports = open("passports.json", "r")
-    passports_str = passports.read()
-    data = json.loads(passports_str)
+    data = open_db("passports.json")
     user = str(ctx.author)
-    passports.close()
     if user in data:
         money = data[user]["coin"]
-        await ctx.send('Деньги: '+str(money))
+        await ctx.send('Деньги: ' + str(money))
     else:
-        add_user = open("passports.json", "w")
         data[user] = {
             'coin': 0
         }
-        data = json.dumps(data)
-        add_user.write(data)
-        add_user.close()
+        update_db("passports.json", data)
         await ctx.send(ctx.author.name + ' Получил паспорт')
+
+
+@bot.command(name='заказ')
+async def drink_order(ctx, drink):
+    data = open_db("database/passports.json")
+    data_bar = open_db("database/bar.json")
+    items = open_db("database/items.json")
+    drinks = items["drinks"]
+    user_name = str(ctx.author)
+    user = data[user_name]
+    if drink in drinks:
+        if user["coin"] >= drinks[drink]["cost"]:
+            order = data_bar["order"]
+            if user_name not in data_bar["order"]:
+                order[user_name] = []
+            order[user_name].append(drink)
+            update_db("bar.json", data_bar)
+            await ctx.send(ctx.author.name + ' заказал ' + drink)
+            return
+    menu = ''
+    for drink_menu in drinks:
+        drink_menu_str = str(drink_menu)
+        menu = menu + drink_menu_str + ': ' + str(drinks[drink_menu_str]["cost"]) + '\n'
+    await ctx.send(ctx.author.name + ': \n' + menu)
 
 
 bot.run(config_bot['token'])
